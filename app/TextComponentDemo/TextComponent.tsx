34c92139acaabc4ac6d9ea09b7b2514b5c79a472
Janki Chauhan <jankichauhan0510@gmail.com>
2026-01-29 12:49:46 +0530

    move code in specific component TextComponent.tsx file 29.1.26 12:49 PM

---
+++ app/TextComponentDemo/TextComponent.tsx
@@ -0,0 +1,42 @@
+import { Text, View } from "react-native";
+
+export default function TextComponent() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#f0f0f0",
+        padding: 20,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 20,
+          color: "blue",
+          fontWeight: "bold",
+        }}
+      >
+        Text with style....
+      </Text>
+
+      <Text>
+        Text with <Text style={{ fontWeight: "bold" }}>Nexted Text</Text>
+      </Text>
+
+      <Text onPress={() => alert("Text clicked!")}>
+        click on me show alert (Text with Click / Touch)
+      </Text>
+
+      <Text numberOfLines={2} ellipsizeMode="tail">
+        <Text style={{ fontWeight: "bold" }}>
+          this is example for not show multiple instead of show one line and
+          remaining with...
+        </Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus iusto
+        dolorum fuga officia vel pariatur aut, optio quibusdam? Nam explicabo
+        nulla veritatis excepturi! Nostrum suscipit exercitationem enim veniam!
+        Veritatis, ullam!
+      </Text>
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import TextComponent from "./TextComponentDemo/TextComponent";
 
 export default function App() {
   return (
@@ -9,34 +10,7 @@ export default function App() {
         padding: 20,
       }}
     >
-      <Text
-        style={{
-          fontSize: 20,
-          color: "blue",
-          fontWeight: "bold",
-        }}
-      >
-        Text with style....
-      </Text>
-
-      <Text>
-        Text with <Text style={{ fontWeight: "bold" }}>Nexted Text</Text>
-      </Text>
-
-      <Text onPress={() => alert("Text clicked!")}>
-        click on me show alert (Text with Click / Touch)
-      </Text>
-
-      <Text numberOfLines={2} ellipsizeMode="tail">
-        <Text style={{ fontWeight: "bold" }}>
-          this is example for not show multiple instead of show one line and
-          remaining with...
-        </Text>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus iusto
-        dolorum fuga officia vel pariatur aut, optio quibusdam? Nam explicabo
-        nulla veritatis excepturi! Nostrum suscipit exercitationem enim veniam!
-        Veritatis, ullam!
-      </Text>
+      <TextComponent />
     </View>
   );
 }
