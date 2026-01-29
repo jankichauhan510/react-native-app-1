37fc82cb5023f204f189d9bf9740d68d6ab4422c
Janki Chauhan <jankichauhan0510@gmail.com>
2026-01-29 12:46:40 +0530

    Text container Demo with different styles 29.1.26 12:46PM

--- app/index.tsx
+++ app/index.tsx
@@ -16,7 +16,26 @@ export default function App() {
           fontWeight: "bold",
         }}
       >
-        Welcome to the React Native App
+        Text with style....
+      </Text>
+
+      <Text>
+        Text with <Text style={{ fontWeight: "bold" }}>Nexted Text</Text>
+      </Text>
+
+      <Text onPress={() => alert("Text clicked!")}>
+        click on me show alert (Text with Click / Touch)
+      </Text>
+
+      <Text numberOfLines={2} ellipsizeMode="tail">
+        <Text style={{ fontWeight: "bold" }}>
+          this is example for not show multiple instead of show one line and
+          remaining with...
+        </Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus iusto
+        dolorum fuga officia vel pariatur aut, optio quibusdam? Nam explicabo
+        nulla veritatis excepturi! Nostrum suscipit exercitationem enim veniam!
+        Veritatis, ullam!
       </Text>
     </View>
   );
