2ae546f1f85c48b7334db0302d5f43639439f44b
Janki Chauhan <jankichauhan0510@gmail.com>
2026-02-02 17:55:53 +0530

    previous commit code is for tab at bottom side like at bottom see tab HOME | Prrofile | Settings 2.2.26 5:55PM

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function HomeScreenofTabNavigation() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home Screen</Text>
