e90dc2070db91ecaccb66423bdffa774b4133b15
Janki Chauhan <jankichauhan0510@gmail.com>
2026-02-03 12:40:02 +0530

    Detail Screen chnage title of detail screen 3.2.26 12:39PM

--- app/details.tsx
+++ app/details.tsx
@@ -4,7 +4,7 @@ import { Button, Text, View } from "react-native";
 export default function Details() {
   return (
     <View>
-      <Text>Details Screen</Text>
+      <Text>Detail Screen</Text>
       <Button title="Go Back" onPress={() => router.back()} />
     </View>
   );
