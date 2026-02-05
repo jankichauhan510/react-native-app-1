6df0db7dba5e53b37432b17ecebd0fc4c744cd7b
Janki Chauhan <jankichauhan0510@gmail.com>
2026-02-05 12:58:58 +0530

    Animation demo for Anumated.view 5.2.26 12:58PM

--- components/APIsDemo/APIsDemoComponent.tsx
+++ components/APIsDemo/APIsDemoComponent.tsx
@@ -1,11 +1,12 @@
 import { View } from "react-native";
-import AlertDemo from "./AlertDemo";
+import AnimatedDemo from "./AnimatedDemo";
 
 export default function APIsDemoComponent() {
   return (
     <View style={{ flex: 1, padding: 8 }}>
       {/* <AccessibilityInfoAPIDemo /> */}
-      <AlertDemo />
+      {/* <AlertDemo /> */}
+      <AnimatedDemo />
     </View>
   );
 }
